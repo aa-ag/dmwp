9144f26de44762a17d2c3a37d4e7e9ac49320bd9
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-09 18:32:45 -0500

    added alternate version for codewars on fractions and link to kata

--- 1. Working with Numbers/codewars_on_fractions.py
+++ 1. Working with Numbers/codewars_on_fractions.py
@@ -1,23 +1,24 @@
+'''
+ https://www.codewars.com/kata/5816f2580e80c5e075000a4f/train/python
+'''
+
+######------ IMPORTS ------######
 from fractions import Fraction
 
 
+######------ FUNCTIONS ------######
 def add_fracs(*args):
-    if not args:
-        return ''
-    s = sum([Fraction(arg) for arg in args])
-
-    if s.denominator == 1:
-        return str(s.numerator)
-    return f"{s.numerator}/{s.denominator}"
+    return str(sum(Fraction(a) for a in args)) if args else ''
 
 
-add_fracs()  # ''
-add_fracs('1/2')  # '1/2'
-add_fracs('1/2', '1/4')  # '3/4'
-add_fracs('1/2', '3/4')  # '5/4'
-add_fracs('2/4', '6/4', '4/4')  # '3'
-add_fracs('2/3', '1/3', '4/6')  # '5/3'
-add_fracs('2/3', '1/4', '5/6')  # '7/4'
-add_fracs('-2/3', '5/3', '-4/6')  # '1/3'
-add_fracs('-7/3', '-1/3', '-2/3')  # '-10/3'
-add_fracs('1/4', '5/4', '-1/2', '-1/1')  # '0'
+######------ TESTS ------######
+print(add_fracs())  # ''
+print(add_fracs('1/2'))  # '1/2'
+print(add_fracs('1/2', '1/4'))  # '3/4'
+print(add_fracs('1/2', '3/4'))  # '5/4'
+print(add_fracs('2/4', '6/4', '4/4'))  # '3'
+print(add_fracs('2/3', '1/3', '4/6'))  # '5/3'
+print(add_fracs('2/3', '1/4', '5/6'))  # '7/4'
+print(add_fracs('-2/3', '5/3', '-4/6'))  # '1/3'
+print(add_fracs('-7/3', '-1/3', '-2/3'))  # '-10/3'
+print(add_fracs('1/4', '5/4', '-1/2', '-1/1'))  # '0'
