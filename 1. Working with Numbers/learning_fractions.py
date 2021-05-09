e417b6ef122b21542adbe9a63325c571a7773fe6
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-09 18:16:25 -0500

    added explanation comments and module python.org page

--- 1. Working with Numbers/learning_fractions.py
+++ 1. Working with Numbers/learning_fractions.py
@@ -1,13 +1,18 @@
 ###--- IMPORTS ---###
 from fractions import Fraction
+# https://docs.python.org/3/library/fractions.html
 
 
 ###--- FUNCTIONS ---###
 def create_a_fraction():
+    # Fraction object has `numberator` and `denominator`
+    # always in this order ^
     f = Fraction(3, 4)
     # print(type(f))  # <class 'fractions.Fraction'>
     print(f)  # 3/4
     print(f + 1 + 1.5)  # 3.25
+
+    print(Fraction(3, 4) + 1 + Fraction(1/4))
 
 
 ###--- DRIVER CODE ---###
