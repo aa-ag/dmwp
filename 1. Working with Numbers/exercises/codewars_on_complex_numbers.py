46a524b3bc2e983f694e1d606df5a599fa5b2887
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-11 23:16:47 -0500

    refactored and added tests for codewars on complex numbers

--- 1. Working with Numbers/exercises/codewars_on_complex_numbers.py
+++ 1. Working with Numbers/exercises/codewars_on_complex_numbers.py
@@ -1,14 +1,14 @@
 class Complex:
-    def __init__(self, re, im):
-        self.real = re
-        self.imaginary = im
+    def __init__(self, a, b):
+        self.real = a
+        self.imaginary = b
 
     def __add__(self, other):
-        return Complex(self.real+other.real, self.imaginary + other.imaginary)
+        return Complex(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        return Complex(self.real*other.real - self.imaginary*other.imaginary,
-                       self.real*other.imaginary + other.real*self.imaginary)
+        return Complex(self.real * other.real - self.imaginary * other.imaginary,
+                       self.real * other.imaginary + other.real * self.imaginary)
 
 
 # TESTS & DRIVER CODE
@@ -22,8 +22,8 @@ print(Complex(2, 0).imaginary)  # 0
 print(Complex(0, 0).imaginary)  # 0
 print(Complex(0, 1).imaginary)  # 1
 
-# add = Complex(1, 0) + Complex(0, 1)
-# print([add.real, add.imaginary])  # [1, 1]
+add = Complex(1, 0) + Complex(0, 1)
+print([add.real, add.imaginary])  # [1, 1]
 
-# m = Complex(2, 2) * Complex(3, 3)
-# print([m.real, m.imaginary])  # [0, 12]
+m = Complex(2, 2) * Complex(3, 3)
+print([m.real, m.imaginary])  # [0, 12]
