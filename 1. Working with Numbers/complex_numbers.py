454395b4e15b213c8f3d28e3fbdb417de4f290a9
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-19 21:42:20 -0500

    added z.real built-in function to complex_numbers.py

--- 1. Working with Numbers/complex_numbers.py
+++ 1. Working with Numbers/complex_numbers.py
@@ -1,20 +1,23 @@
-a = 2 - 3j
-print(type(a))
+# a = 2 - 3j
+# print(type(a))
 # <class 'complex'>
 
-a = complex(2, 3)
-print(a)
+# a = complex(2, 3)
+# print(a)
 # (2+3j)
 
-b = 3 + 3j
-print(a + b)
+# b = 3 + 3j
+# print(a + b)
 # (5+6j)
 
-print(a - b)
+# print(a - b)
 # (-1+0j)
 
-print(a * b)
+# print(a * b)
 # (-3+15j)
 
-print(a / b)
+# print(a / b)
 # (0.8333333333333334+0.16666666666666666j)
+
+z = 2 + 3j
+print(z.real)
