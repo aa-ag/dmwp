40ff3135a88981a60abab38049f3469b30957d21
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-19 21:53:51 -0500

    added cmath library reference and a few builtin funcionalities in complex_numbers.py

--- 1. Working with Numbers/complex_numbers.py
+++ 1. Working with Numbers/complex_numbers.py
@@ -19,12 +19,26 @@
 # print(a / b)
 # (0.8333333333333334+0.16666666666666666j)
 
-z = 2 + 3j
+# z = 2 + 3j
 # print(z.real)
 # print(z.imag)
 # print(z.conjugate())
 # the magnitude of a complex number
-print((z.real ** 2 + z.imag ** 2) ** 0.5)  # 3.605551275463989
+# print((z.real ** 2 + z.imag ** 2) ** 0.5)  # 3.605551275463989
 # or
-print(abs(z))  # 3.605551275463989
-print(abs(z) == (z.real ** 2 + z.imag ** 2) ** 0.5)  # True
+# print(abs(z))  # 3.605551275463989
+# print(abs(z) == (z.real ** 2 + z.imag ** 2) ** 0.5)  # True
+
+############------------ cmath library ------------############
+# https://docs.python.org/3/library/cmath.html
+import cmath
+
+
+print(cmath.phase(complex(-1.0, 0.0)))
+# 3.141592653589793
+
+print(cmath.pi)
+# 3.141592653589793
+
+print(cmath.inf)
+# inf
