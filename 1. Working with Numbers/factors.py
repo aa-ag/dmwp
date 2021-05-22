e04c66c4b4dfe6ab60f65c0b6547d98130658fed
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-21 23:05:17 -0500

    added docstring to factors.py

--- 1. Working with Numbers/factors.py
+++ 1. Working with Numbers/factors.py
@@ -1,4 +1,9 @@
 def is_factor(a, b):
+    '''
+     when a non-zero integer "a" divides
+     another integer "b", leaving a remainder 
+     of zero, "a" is said to be a factor of "b"
+    '''
     if b % a == 0:
         return True
     return False
