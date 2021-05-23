4d75abc068460b6df56b6381abf35cba459c4ce7
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-22 23:11:22 -0500

    added conversions from inches to meters, and farenheit to celcius and vice versa

--- 1. Working with Numbers/measurement_units.py
+++ 1. Working with Numbers/measurement_units.py
@@ -2,3 +2,15 @@
 # (n * 2.54) / 100
 # example:
 print((5 * 2.54) / 100)
+# 0.127
+
+
+# farenheit to celsius
+# (F - 32) * (5/9)
+print((10 - 32) * (5/9))
+# -12.222222222222223
+
+# celsius to farenheit
+# C * (9/5) + 32
+print(10 * (9/5) + 32)
+# 50.0
