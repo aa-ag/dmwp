263e1c5344f6dff985e4f16d3a48e3990abaa532
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-23 20:05:18 -0500

    added quit optin in while loop within driver code in km/miles conversion program and made print_menu() function be one line instead of two separate print statements

--- 1. Working with Numbers/measurement_units.py
+++ 1. Working with Numbers/measurement_units.py
@@ -1,27 +1,32 @@
 def print_menu():
-    print('Enter 1 to convert Kilometers to Miles')
-    print('Enter 2 to convert Kilometers to Miles')
+    print('''Enter 1 to convert kilometers to miles\n2 to convert miles to kilometers\n"q" to quit\exit''')
 
 
 def kilometer_to_miles():
     kilometers = input('Enter number of kilometers:\n')
-    return f"\n{round(int(kilometers) / 1.609, 2)} miles"
+    return f"\n{kilometers} kilometers equals {round(int(kilometers) / 1.609, 2)} miles."
 
 
 def miles_to_kilometers():
     miles = input('Enter number of miles:\n')
-    return f"\n{round(int(miles) * 1.609, 2)} kilometers"
+    return f"\n{miles} miles equals {round(int(miles) * 1.609, 2)} kilometers."
 
 
 if __name__ == '__main__':
     print_menu()
 
-    option = input('Which conversion would you like to do?\n')
-    if option == '1':
-        print(kilometer_to_miles())
+    while True:
+        option = input('\nWhich conversion would you like to do?\n')
 
-    if option == '2':
-        print(miles_to_kilometers())
+        if option == 'q':
+            print('\nbye.')
+            break
+
+        if option == '1':
+            print(kilometer_to_miles())
+
+        if option == '2':
+            print(miles_to_kilometers())
 
 
 # inches to meters
