b7af466fda28a18fee963cf9e17c0912ba13119d
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-24 22:11:51 -0500

    added even-odd_vending_machine.py

---
+++ 1. Working with Numbers/exercises/eve-odd_vending_machine.py
@@ -0,0 +1,20 @@
+######------ INSTRUCTIONS ------######
+'''
+Try writing an "even-odd vending machine", 
+which will take a number as input and do two things:
+(1) Print whether the number is even or odd.
+(2) Display the number followed by the next 9 even or odd numbers
+'''
+######------ SOLUTION ------######
+# https://nostarch.com/doingmathwithpython
+
+
+######------ FUNCTIONS ------######
+def even_odd_vending_machine():
+    pass
+
+
+######------ TESTS ------######
+
+
+######------ DRIVER CODE ------######
