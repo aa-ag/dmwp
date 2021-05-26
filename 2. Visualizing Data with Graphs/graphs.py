f8a47fb52ee9597e3bbd2c7ae27ddcc39521a362
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-25 23:21:08 -0500

    added savefig() function to create png image of table created

--- 2. Visualizing Data with Graphs/graphs.py
+++ 2. Visualizing Data with Graphs/graphs.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 
 ############------------ FUNCTIONS ------------############
 def create_graph():
+    '''
+     savefig before show()
+     https://stackoverflow.com/questions/9012487/matplotlib-pyplot-savefig-outputs-blank-image
+    '''
     x_numbers = [1, 2, 3]
     y_numbers = [2, 4, 6]
 
     plt.plot(x_numbers, y_numbers)
+    plt.savefig('test.png')
     plt.show()
 
 
--- README.md
+++
@@ -1,3 +0,0 @@
-# Doing Math with Python
-
-Will go through [Doing Math with Python](https://doingmathwithpython.github.io/) on this repo.
