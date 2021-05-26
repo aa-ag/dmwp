0e684055406d0101f72d4ed2f127e78cf9df8a65
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-25 23:14:47 -0500

    refactored graphs.py into function version

--- 2. Visualizing Data with Graphs/graphs.py
+++ 2. Visualizing Data with Graphs/graphs.py
@@ -1,4 +1,16 @@
+############------------ IMPORTS ------------############
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3], [2, 4, 6])
-plt.show()
+
+############------------ FUNCTIONS ------------############
+def create_graph():
+    x_numbers = [1, 2, 3]
+    y_numbers = [2, 4, 6]
+
+    plt.plot(x_numbers, y_numbers)
+    plt.show()
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == '__main__':
+    create_graph()
