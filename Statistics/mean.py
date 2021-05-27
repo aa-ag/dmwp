e394e156655ed85c4f2922aa183ad6e60eff50d1
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-26 20:29:34 -0500

    fixed typo in test_cases title

--- Statistics/mean.py
+++ Statistics/mean.py
@@ -3,7 +3,7 @@ def calculate_mean(l):
     return sum(l) / len(l)
 
 
-######------ DRIVER CODE ------######
+######------ TEST CASES ------######
 def test_case_1():
     short_list = [1, 2, 3, 4, 5]
     return calculate_mean(short_list)
