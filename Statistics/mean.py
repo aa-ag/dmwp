e785f664699307d53f16d57f348d8f8b0dd00e10
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-26 20:26:52 -0500

    added short_list / test_case_1() function to mean.py

--- Statistics/mean.py
+++ Statistics/mean.py
@@ -1,8 +1,14 @@
 ######------ FUNCTIONS ------######
-def calculate_mean():
-    pass
+def calculate_mean(l):
+    return sum(l) / len(l)
+
+
+######------ DRIVER CODE ------######
+def test_case_1():
+    short_list = [1, 2, 3, 4, 5]
+    return calculate_mean(short_list)
 
 
 ######------ DRIVER CODE ------######
 if __name__ == '__main__':
-    pass
+    print(test_case_1())
