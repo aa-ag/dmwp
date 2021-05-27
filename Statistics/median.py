41ab3bc75fa241408e5303ce704f5e430fdd8070
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-26 20:32:04 -0500

    added median.py and its boilerplate code

---
+++ Statistics/median.py
@@ -0,0 +1,13 @@
+######------ FUNCTIONS ------######
+def calculate_median():
+    pass
+
+
+######------ TEST CASES ------######
+def test_case_1():
+    pass
+
+
+######------ DRIVER CODE ------######
+if __name__ == '__main__':
+    pass
