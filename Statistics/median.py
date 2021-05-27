251d527372ed32e67e84837721e02c4bee60dda6
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-26 20:39:23 -0500

    added logic for calculating median in median.py, passing test

--- Statistics/median.py
+++ Statistics/median.py
@@ -1,13 +1,22 @@
 ######------ FUNCTIONS ------######
-def calculate_median():
-    pass
+def calculate_median(l):
+    length = len(l)
+    l.sort()
+    if length % 2 == 0:
+        even_m1 = int(length / 2) - 1
+        even_m2 = int((length / 2) + 1) - 1
+
+        return (l[even_m1] + l[even_m2]) / 2
+    else:
+        odd_m = int((length + 1) / 2) - 1
+        return l[odd_m]
 
 
 ######------ TEST CASES ------######
 def test_case_1():
-    pass
+    return calculate_median([100, 60, 70, 900, 100, 200, 500, 500, 503, 600, 1000, 1200])
 
 
 ######------ DRIVER CODE ------######
 if __name__ == '__main__':
-    pass
+    print(test_case_1())
