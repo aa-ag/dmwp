a7b4e8e116f701ff23b94ff45fcbc9c06663668f
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-28 16:01:13 -0500

    entire function in correlation.py with one test case in driver code

--- Statistics/correlation.py
+++ Statistics/correlation.py
@@ -2,16 +2,26 @@
 def measure_correlation(x, y):
     n = len(x)
 
-    product = [(i * j) for i, j in zip(x, y)]
+    product_sum = sum([(i * j) for i, j in zip(x, y)])
 
     squared_x_sum = sum(x) ** 2
     squared_y_sum = sum(y) ** 2
 
-    print(squared_x_sum, squared_y_sum)
+    x_square_sum = sum([(xi ** 2) for xi in x])
+    y_square_sum = sum([(yi ** 2) for yi in y])
+
+    numerator = (n * product_sum) - (squared_x_sum * squared_y_sum)
+
+    denominator_x_half = n * x_square_sum - squared_x_sum
+    denominator_y_half = n * y_square_sum - squared_y_sum
+    denominator = (denominator_x_half * denominator_y_half) ** 0.5
+
+    correlation = numerator / denominator
+    return correlation
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     list_1 = [1, 2, 3]
     list_2 = [4, 5, 6]
-    measure_correlation(list_1, list_2)
+    print(measure_correlation(list_1, list_2))
