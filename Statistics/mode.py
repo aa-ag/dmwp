f4b748b8f9ef8bd321ca9147169ef9c2d6efecd7
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-27 20:53:43 -0500

    refactored mode.py into function with driver code

--- Statistics/mode.py
+++ Statistics/mode.py
@@ -1,5 +1,10 @@
 from collections import Counter
 
-simple_list = [1, 2, 3, 4, 5, 2]
-mode = Counter(simple_list)
-print(mode.most_common(1)[0][0])
+
+def calculate_mode(l):
+    mode = Counter(l)
+    return mode.most_common(1)[0][0]
+
+
+if __name__ == '__main__':
+    print(calculate_mode([1, 2, 3, 4, 5, 2, 8, 9, 2, 3, 4, 2, 8]))
