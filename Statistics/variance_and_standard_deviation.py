cc4e5f800808aec782e6f728ba7fbb05cda7b7af
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-28 15:28:55 -0500

    refactored vasd code to include at least two test cases and added test functions separately to have cleaner driver code

--- Statistics/variance_and_standard_deviation.py
+++ Statistics/variance_and_standard_deviation.py
@@ -28,13 +28,42 @@ def find_variance(numbers):
 
     return variance
 
+############------------ TESTS ------------############
 
-############------------ DRIVER CODE ------------############
-if __name__ == "__main__":
+
+def test_case_1():
+    '''
+     testing list with high variance, 
+     and high standard deviation
+    '''
     numbers = [100, 60, 70, 900, 100, 200, 500, 500, 503, 600, 1000, 1200]
     variance = find_variance(numbers)
     print(f"The variance of this list of numbers is {round(variance, 2)}")
+    # The variance of this list of numbers is 141047.35
 
     standard_deviation = variance ** 0.5
     print(
         f"The standard deviation of the list is {round(standard_deviation, 2)}")
+    # The standard deviation of the list is 375.56
+
+
+def test_case_2():
+    '''
+     testing list with low variance, 
+     and low standard deviation
+    '''
+    numbers = [382, 389, 377, 397, 396, 368, 369, 392, 398, 367, 396]
+    variance = find_variance(numbers)
+    print(f"The variance of this list of numbers is {round(variance, 2)}")
+    # The variance of this list of numbers is 141.87
+
+    standard_deviation = variance ** 0.5
+    print(
+        f"The standard deviation of the list is {round(standard_deviation, 2)}")
+    # The standard deviation of the list is 11.91
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    test_case_1()
+    test_case_2()
