123a68b4adb2505c6a3ab95638b697ed37907b4b
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-27 21:03:51 -0500

    refactored mode.py to find mode(s) in case there are more than one

--- Statistics/mode.py
+++ Statistics/mode.py
@@ -2,9 +2,20 @@ from collections import Counter
 
 
 def calculate_mode(l):
-    mode = Counter(l)
-    return mode.most_common(1)[0][0]
+    count = Counter(l)
+    frequencies = count.most_common()
+    most_repeated_number = frequencies[0][1]
+
+    modes = list()
+
+    for number in frequencies:
+        if number[1] == most_repeated_number:
+            modes.append(number[0])
+    return modes
 
 
 if __name__ == '__main__':
-    print(calculate_mode([1, 2, 3, 4, 5, 2, 8, 9, 2, 3, 4, 2, 8]))
+    m = calculate_mode([5, 5, 5, 4, 4, 4, 9, 1, 3])
+    print("The mode(s) in the list are:")
+    for i in m:
+        print(i)
