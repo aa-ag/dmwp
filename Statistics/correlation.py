fa3de70fa1fb6bbfa49b778e49bc5aabcf437016
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-28 15:49:37 -0500

    added product calculation part of measuring correlation in correlation.py

--- Statistics/correlation.py
+++ Statistics/correlation.py
@@ -1,8 +1,14 @@
 ############------------ FUNCTIONS ------------############
-def measure_correlation():
-    pass
+def measure_correlation(x, y):
+    n = len(x)
+
+    product = [(i * j) for i, j in zip(x, y)]
+
+    print(product)
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    list_1 = [1, 2, 3]
+    list_2 = [4, 5, 6]
+    measure_correlation(list_1, list_2)
