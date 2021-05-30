8e21649368b1edaa1360374df34c1cba66e03383
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-29 22:10:53 -0500

    fixed symbols assignment input and printed s, testing how mathematical operations are treated

--- 4.Sympy/intro.py
+++ 4.Sympy/intro.py
@@ -1,7 +1,6 @@
 from sympy.core.symbol import Symbol, symbols
 
-x = Symbol('x')
-y = Symbol('y')
-z = Symbol('z')
-
-x, y, z = symbols('x', 'y', 'z')
+x, y, z = symbols('x,y,z')
+s = x*y + x*y
+print(s)
+# 2*x*y
