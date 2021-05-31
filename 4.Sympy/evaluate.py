801280a772911cb2bf731a978b612c42e912ba99
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-31 13:10:20 -0500

    added code for example in which one of the values is substituted for a value that is relative to the other

--- 4.Sympy/evaluate.py
+++ 4.Sympy/evaluate.py
@@ -5,6 +5,12 @@ y = Symbol('y')
 
 expression = x*x + x*y + x*y + y*y
 
-evaluated_answer_with_values = expression.subs({x: 1, y: 2})
-print(evaluated_answer_with_values)
+# simple substitution
+# evaluated_answer_with_values = expression.subs({x: 1, y: 2})
+# print(evaluated_answer_with_values)
 # 9
+
+# one value in terms of another
+evaluated_answer_with_values = expression.subs({x: 1-y})
+print(evaluated_answer_with_values)
+# y**2 + 2*y*(1 - y) + (1 - y)**2
