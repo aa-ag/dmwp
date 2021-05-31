4f1fde52fedd95e01cd0c03995e001c7eb0bf61f
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-31 13:03:37 -0500

    added code for series.py

--- 4.Sympy/series.py
+++ 4.Sympy/series.py
@@ -3,9 +3,24 @@ from sympy import pprint, init_printing
 
 
 def print_series(n):
-    pass
+    init_printing(order="rev-lex")
+
+    x = Symbol('x')
+
+    series = x
+
+    for i in range(2, n+1):
+        series = series + (x**i)/i
+    pprint(series)
 
 
 if __name__ == '__main__':
     n = input("Enter the number of terms you want in the series:\n")
-    print_series()
+    print_series(int(n))
+
+    '''
+            2    3    4    5
+        x    x    x    x 
+    x + ── + ── + ── + ──
+        2    3    4    5 
+    '''
