153d44f3406711db1f59fc4a7719a940e380ce72
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-31 13:00:07 -0500

    added series.py in Sympy folder and it boilerplate code

---
+++ 4.Sympy/series.py
@@ -0,0 +1,11 @@
+from sympy.core.symbol import Symbol
+from sympy import pprint, init_printing
+
+
+def print_series(n):
+    pass
+
+
+if __name__ == '__main__':
+    n = input("Enter the number of terms you want in the series:\n")
+    print_series()
