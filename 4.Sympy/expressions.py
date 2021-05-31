d47998847cd242fc38b5e214cddacceef1840058
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-30 21:49:00 -0500

    added use of expand() builtin function from sympy in expressions.py

--- 4.Sympy/expressions.py
+++ 4.Sympy/expressions.py
@@ -1,3 +1,4 @@
+from sympy.core.function import expand
 from sympy.core.symbol import Symbol
 from sympy import factor
 
@@ -5,5 +6,7 @@ x = Symbol('x')
 y = Symbol('y')
 
 expression = (x ** 2) - (y ** 2)
-print(factor(expression))
-# (x - y)*(x + y)
+
+factors = factor(expression)
+print(expand(factors))
+# x**2 - y**2
