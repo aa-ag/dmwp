209c0edd848befa84c95c4a906f29fd6c5da2f0c
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-30 22:00:44 -0500

    added pprint import and example to expressions.py

--- 4.Sympy/expressions.py
+++ 4.Sympy/expressions.py
@@ -1,15 +1,13 @@
 from sympy.core.function import expand
 from sympy.core.symbol import Symbol
-from sympy import factor
+from sympy import factor, pprint
 
 x = Symbol('x')
 y = Symbol('y')
 
 expr = x**3 + 3*x**2*y + 3*x*y**2 + y**3
-
-factors = factor(expr)
-print(factors)
-# (x + y)**3
-
-print(expand(factors))
-# x**3 + 3*x**2*y + 3*x*y**2 + y**3
+pprint(expr)
+'''
+  3      2          2    3
+x  + 3⋅x ⋅y + 3⋅x⋅y  + y 
+'''
