9caad31db530138d520d97c2ea423db2e8000617
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-04 22:36:52 -0500

    added quadratic equation example using complex number in equations.py

--- 4.Sympy/equations.py
+++ 4.Sympy/equations.py
@@ -3,9 +3,7 @@ from sympy.core import Symbol
 from sympy.solvers.solvers import solve
 
 x = Symbol('x')
-expression = x**2 + 5*x + 4
-print(solve(expression))
-# [-4, -1]
+expression = x**2 + x + 1
 
 print(solve(expression, dict=True))
-# [{x: -4}, {x: -1}]
+# [{x: -1/2 - sqrt(3)*I/2}, {x: -1/2 + sqrt(3)*I/2}]
