ae4d5e7feee982f27cca51666cb2330824974ea5
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-12 22:52:40 -0500

    added code from class for Different Gravity, Different Results

--- 5. Probability/gravity.py
+++ 5. Probability/gravity.py
@@ -1,7 +1,15 @@
 from sympy import FiniteSet, pi
 
-def time_period():
-    pass
+def time_period(length, g):
+    return 2 * pi * (length/g) ** 0.5
+
 
 if __name__ == "__main__":
-    time_period()
+    l = FiniteSet(15, 18, 21, 22.5, 25)
+    g_values = FiniteSet(9.8, 9.78, 9.83)
+    print('{0:^15}{1:^15}{2:^15}'.format('Length(cm)', 'Gravity(m/s^2)', 'Time Period(s)'))
+    for i in l * g_values:
+        le = i[0]
+        g = i[1]
+        t = time_period(1/100, g)
+        print('{0:^15}{1:^15}{2:^15.3f}'.format(float(le), float(g), float(t)))
