9e18392cc2d5779c85478691bbc056425cbe049f
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-16 20:10:56 -0500

    added logic to check if n is prime

--- 5. Probability/rolling_a_die.py
+++ 5. Probability/rolling_a_die.py
@@ -1,8 +1,15 @@
 def probability(space, event):
     return len(event) / len(space)
 
-def check_prime():
-    pass
+def check_prime(n):
+    if n != 1:
+        for factor in range(2, n):
+            if n % factor == 0:
+                return False
+    else:
+        return False
+    return True
+
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
