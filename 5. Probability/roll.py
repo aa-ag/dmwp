c6d8607a85fbd530504ebac6dd75746680cd5403
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-17 18:01:51 -0500

    added imports, function and driver code for roll.py

--- 5. Probability/roll.py
+++ 5. Probability/roll.py
@@ -1,3 +1,23 @@
 '''
 Roll a die until the total score is N
 '''
+
+import matplotlib.pyplot as plt
+import random
+
+
+target_score = 20
+
+def roll():
+    return random.randint(1, 6)
+
+if __name__ == "__main__":
+    score = 0
+    rolls = 0
+    while score < target_score:
+        roll_the_die = roll()
+        rolls += 1
+        print(f"rolled {roll_the_die}")
+        score += roll_the_die
+
+    print(f"score of {score} reached after {rolls} rolls")
