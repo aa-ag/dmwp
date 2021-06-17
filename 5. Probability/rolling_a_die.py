2d5bf651b559a0c58e669b6b936338fb9122c2a9
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-16 20:18:55 -0500

    added results in comment (rolling_a_die.py)

--- 5. Probability/rolling_a_die.py
+++ 5. Probability/rolling_a_die.py
@@ -26,3 +26,9 @@ if __name__ == "__main__":
     print(f"Sample space: {space}")
     print(f"Event: {event}")
     print(f"The probability of rolling a prime: {probability_calculation}")
+
+'''
+Sample space: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+Event: {2, 3, 5, 7, 11, 13, 17, 19}
+The probability of rolling a prime: 0.4
+'''
