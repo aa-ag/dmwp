e94b2995453cbd8a897fef9c3872cdd4761c232b
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-16 20:20:34 -0500

    added %.2f % logic to display trailing zero for probability float

--- 5. Probability/rolling_a_die.py
+++ 5. Probability/rolling_a_die.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
 
     print(f"Sample space: {space}")
     print(f"Event: {event}")
-    print(f"The probability of rolling a prime: {probability_calculation}")
+    print("The probability of rolling a prime: " + "%.2f" % probability_calculation)
 
 '''
 Sample space: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 Event: {2, 3, 5, 7, 11, 13, 17, 19}
-The probability of rolling a prime: 0.4
+The probability of rolling a prime: 0.40
 '''
