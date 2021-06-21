7b1d223296a73ed2e909df5e35c6944503a1f09c
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-20 19:02:13 -0500

    added uniform.py and its base code (function toss()) from class

---
+++ 5. Probability/uniform.py
@@ -0,0 +1,15 @@
+############------------ IMPORT(S) ------------############
+import random
+
+############------------ FUNCTION(S) ------------############
+def toss():
+    '''
+     simulate a toss where
+     heads is twice as likely as
+     tales
+    '''
+    pass
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    toss()
