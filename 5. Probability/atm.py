e6184f9a003030cc0bb768c85b28b13c2ea9ff94
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-21 21:46:45 -0500

    added atm.py and its base code from class with docstring

---
+++ 5. Probability/atm.py
@@ -0,0 +1,15 @@
+############------------ IMPORT(S) ------------############
+import random
+
+############------------ FUNCTION(S) ------------############
+def get_index():
+    '''
+     simulate an ATM that dispenses 
+     dollar bills of differente
+     denominations
+    '''
+    pass
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    pass
