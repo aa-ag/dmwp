6f7959ca94935e98cfb7bf26f75dda55b737a64a
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-21 21:51:58 -0500

    added logic under get_index() in atm.py

--- 5. Probability/atm.py
+++ 5. Probability/atm.py
@@ -2,13 +2,27 @@
 import random
 
 ############------------ FUNCTION(S) ------------############
-def get_index():
+def get_index(probability):
     '''
      simulate an ATM that dispenses 
      dollar bills of differente
      denominations
     '''
-    pass
+    c_probability = 0
+
+    sum_probability = list()
+
+    for p in probability:
+        c_probability += p
+        sum_probability.append(c_probability)
+
+    r = random.random()
+
+    for index, sp in enumerate(sum_probability):
+        if r <= sp:
+            return index
+
+    return len(probability) - 1
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
