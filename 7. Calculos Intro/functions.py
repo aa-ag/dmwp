bde9e9f4921e330985b3c674a140c84744f3a363
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-24 19:41:59 -0500

    added functions.py within newly added Calculus folder

---
+++ 7. Calculos Intro/functions.py
@@ -0,0 +1,13 @@
+'''
+ a function is a mapping between an input set and an output set. 
+ the special condition of a function is that an element of the input set
+ is related to exactly one element of the output set.
+
+** to do: https://docs.python.org/3/library/math.html
+'''
+############------------ IMPORT(S) ------------############
+import math 
+
+############------------ CODE ------------############
+print(math.sin(math.pi/2))
+# 1.0
