4981ee08f3c6231143b6d018f9740e1adba5eb11
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-24 20:00:17 -0500

    added logic to calculate compoinded interest

--- 7. Calculus Intro/interest.py
+++ 7. Calculus Intro/interest.py
@@ -4,6 +4,17 @@ from sympy import Limit, Symbol, S
 ############------------ CODE ------------############
 n = Symbol('n')
 
-limit = Limit((1+1/n)**n, n, S.Infinity).doit()
+# limit = Limit((1+1/n)**n, n, S.Infinity).doit()
+# print(limit)
+# # E
+
+principal = Symbol('p')
+
+rate = Symbol('r')
+
+time = Symbol('t')
+
+limit = Limit(principal * (1 + rate/n) ** (n * time), n, S.Infinity).doit()
+
 print(limit)
-# E
+# p*exp(r*t)
